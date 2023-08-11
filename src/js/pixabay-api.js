b3025a5354862a7018ecd98a8e5d1b5ff826d3b7
nicksolony <nicksolony@gmail.com>
2023-08-11 18:19:53 -0400

    set up HTTP request params

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,9 +2,13 @@ import axios from "axios";
 
 let API_KEY = '38783667-102dd7a1eb53387882edbc878';
 let BASE_URL = 'https://pixabay.com/api/';
+let IMAGE_TYPE = 'photo';
+let ORIENT = 'horizontal';
+let SAFE_SEARCH = 'true';
 
 function fetchPhotos(searchQuery) {
-    let url = `${BASE_URL}?key=${API_KEY}&q${searchQuery}`;
+    let url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=${IMAGE_TYPE}&orientation=${ORIENT}&safesearch=${SAFE_SEARCH}`;
+    console.log(url);
     return axios({
         baseURL: url,
         method: 'GET',
@@ -16,3 +20,6 @@ function fetchPhotos(searchQuery) {
 
 export default fetchPhotos;
 
+
+
+// https://pixabay.com/api/?key=38783667-102dd7a1eb53387882edbc878&q=yellow+flowers&image_type=photo
