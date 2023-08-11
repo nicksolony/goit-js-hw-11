a60c7b0ec6a23b1b405093453e2064321ae23466
nicksolony <54452819+nicksolony@users.noreply.github.com>
2023-08-11 20:10:40 +0000

    Deploying to gh-pages from @ nicksolony/goit-js-hw-11@65de8fcd79eb6d9f94d52778d081efa0cf5e8006 🚀

---
+++ index.8cd154f1.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault(),console.log(e.target.searchQuery.value)}));
+//# sourceMappingURL=index.8cd154f1.js.map
---
+++ index.dbac1008.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",(e=>{e.preventDefault(),console.log(e.target.searchQuery.value)}));
+//# sourceMappingURL=index.dbac1008.js.map
