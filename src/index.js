e28731399e9e9ff668e0814c45fd7d4f83c1a0d7
nicksolony <nicksolony@gmail.com>
2023-08-13 12:13:33 -0400

    added load more functionality

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import imgTpl from './hbs/img.hbs';
 
 let formEl = document.querySelector(`#search-form`);
 let galleryEl = document.querySelector('.gallery');
+let loadMoreBtn = document.querySelector('.load-more')
 
 const template = Handlebars.compile("Name: {{name}}");
 
@@ -16,6 +17,8 @@ Notify.init({
     clickToClose: true
 })
 
+hideLoadMore();
+
 let pixabayApi = new PixabayApi();
 
 formEl.addEventListener(`submit`, (e) => {
@@ -23,12 +26,15 @@ formEl.addEventListener(`submit`, (e) => {
     searchImg(e.target.searchQuery.value)
 })
 
+loadMoreBtn.addEventListener('click',loadMorePhotos)
+
 async function searchImg(searchQuery) {
     try {
         pixabayApi.query = searchQuery;
 
         pixabayApi.resetPage();
         clearGallery();
+        showLoadMore();
        let result = await pixabayApi.fetchPhotos();
        galleryEl.insertAdjacentHTML('beforeend', imgTpl(result))
        if (!result[0]) {
@@ -42,4 +48,27 @@ async function searchImg(searchQuery) {
 
 function clearGallery() {
     galleryEl.innerHTML="";
+};
+
+
+function showLoadMore() {
+    loadMoreBtn.classList.remove('is-hidden');
+};
+
+function hideLoadMore() {
+    loadMoreBtn.classList.add('is-hidden');
+};
+
+async function loadMorePhotos() {
+    console.log('click');
+    try {
+       let result = await pixabayApi.fetchPhotos();
+       galleryEl.insertAdjacentHTML('beforeend', imgTpl(result))
+       if (!result[0]) {
+        Notify.failure(`Sorry, we've loaded all images`)
+       }
+       console.log(result);   
+    } catch (err) {
+        Notify.failure(`We're sorry, an error has occurred. Please reload this page and try again.`);
+    };
 }
